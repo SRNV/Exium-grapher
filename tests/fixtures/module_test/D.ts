67158cf8bcc9c53d6a2a462f09e23dc2f25258a9
SRNV <alula.rudy@gmail.com>
2021-08-12 11:47:00 +0200

    lint

--- tests/fixtures/module_test/D.ts
+++ tests/fixtures/module_test/D.ts
@@ -1,1 +1,1 @@
-const a = 10;
+export const a = 10;
--- tests/module_test.ts
+++ tests/module_test.ts
@@ -1,9 +1,7 @@
 import { compute } from '../src/mod.ts';
 import {
-  assert,
   assertStrictEquals
 } from "https://deno.land/std@0.95.0/testing/asserts.ts";
-import { ExiumGrapherModel } from "../src/ExiumGrapherModel.ts";
 import reader from '../src/reader.ts';
 
 Deno.test('exium-grapher - can resolve module path', async () => {
